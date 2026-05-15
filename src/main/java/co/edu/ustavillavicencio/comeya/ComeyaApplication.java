08dfbbf520c0ffd735934d3ce721b5524a038e5d
DiegoBarbosaDeveloper <diegobarbosa090305@gmail.com>
2026-05-15 08:51:14 -0500

    initial commit

---
+++ src/main/java/co/edu/ustavillavicencio/comeya/ComeyaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ustavillavicencio.comeya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComeyaApplication.class, args);
+	}
+
+}
