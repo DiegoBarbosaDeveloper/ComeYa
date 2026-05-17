4156f827ebc32376cd27c37711f65e60568415b0
Angel <pepepe8999999@gmail.com>
2026-05-16 19:39:58 -0500

    agregando los atributos en proceso

--- src/main/java/co/edu/ustavillavicencio/comeya/model/entity/CafeteriaEntity.java
+++ src/main/java/co/edu/ustavillavicencio/comeya/model/entity/CafeteriaEntity.java
@@ -1,4 +1,24 @@
 package co.edu.ustavillavicencio.comeya.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+@Entity
+@Table(name="usta_cafeteria")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CafeteriaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+    @Column(name = "abierto")
+    private boolean abierto;
+    @Column(name = "sirve_comida")
+    private boolean sirveComida;
 }
--- src/main/java/co/edu/ustavillavicencio/comeya/model/entity/StaffcafeteriaEntity.java
+++ src/main/java/co/edu/ustavillavicencio/comeya/model/entity/StaffcafeteriaEntity.java
@@ -1,4 +1,22 @@
 package co.edu.ustavillavicencio.comeya.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+@Entity
+@Table(name = "usta_cafeteria_staff")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class StaffcafeteriaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
