7cbbc2772995db91bec71881a6ee6dae0d24fd6e
DiegoBarbosaDeveloper <diegobarbosa090305@gmail.com>
2026-05-22 08:10:37 -0500

    Changed: JwtFilter.java

--- src/main/java/co/edu/ustavillavicencio/comeya/security/JwtFilter.java
+++ src/main/java/co/edu/ustavillavicencio/comeya/security/JwtFilter.java
@@ -5,7 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,7 +24,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
     
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException, java.io.IOException {
 
         String authHeader = request.getHeader("Authorization");
