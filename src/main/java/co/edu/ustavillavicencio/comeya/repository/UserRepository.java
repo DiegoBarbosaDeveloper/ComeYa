1cd8b06132325044fbf842c8d548557c11bec714
Diego Barbosa <diegobarbosa090305@gmail.com>
2026-05-24 18:05:24 -0500

    Rename methods to use 'findByName' and 'existsByName'

--- src/main/java/co/edu/ustavillavicencio/comeya/repository/UserRepository.java
+++ src/main/java/co/edu/ustavillavicencio/comeya/repository/UserRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByName(String username);
  
     Optional<UserEntity> findByEmail(String email);
  
-    boolean existsByUsername(String username);
+    boolean existsByName(String username);
  
     boolean existsByEmail(String email);
 }
