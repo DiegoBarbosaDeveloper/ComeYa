7411efa8e838f809717771a7f424934cc401b2f6
Diego Barbosa <diegobarbosa090305@gmail.com>
2026-05-24 18:09:46 -0500

    Delete src/main/java/co/edu/ustavillavicencio/comeya/service/AuthService.java

--- src/main/java/co/edu/ustavillavicencio/comeya/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package co.edu.ustavillavicencio.comeya.service;
-
-import co.edu.ustavillavicencio.comeya.dto.login.LoginRequest;
-import co.edu.ustavillavicencio.comeya.dto.login.LoginResponse;
-
-public interface AuthService {
-
-    LoginResponse login(LoginRequest request);
-}
